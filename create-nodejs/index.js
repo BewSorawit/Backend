25c276be8260176663e87891d89e49c34d3454f9
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-06 14:09:45 +0700

    nodemon

--- create-nodejs/index.js
+++ create-nodejs/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     const myhtml = `
     <h1>Hello Node.js</h1>
-    <p>Brown studio | 24</p>`
+    <p style="color:red">Brown studio  24</p>`
     res.write(myhtml)
     res.end()
 }).listen(3000, 'localhost', () => {
