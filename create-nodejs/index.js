924b432597c9b88d5a07b1f3da26a2f707e377d2
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-06 14:44:05 +0700

    webpages

--- create-nodejs/index.js
+++ create-nodejs/index.js
@@ -1,13 +1,17 @@
 const http = require('http')
+const fs = require('fs')
+
+const indexPage = fs.readFileSync(`${__dirname}/webpages/index.html`, 'utf-8')
+const productPage = fs.readFileSync(`${__dirname}/webpages/product.html`, 'utf-8')
 
 const server = http.createServer((req, res) => {
     const pathName = req.url
     console.log("url = ", pathName);
-    if(pathName ==="/" || pathName === "/home"){
-        res.end("Hello Brown")
-    }else if(pathName==="/product"){
-        res.end("Hello Project")
-    }else{
+    if (pathName === "/" || pathName === "/home") {
+        res.end(indexPage)
+    } else if (pathName === "/product") {
+        res.end(productPage)
+    } else {
         res.writeHead(404)
         res.end("Not Found")
     }
