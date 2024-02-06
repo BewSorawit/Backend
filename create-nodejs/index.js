697a9e49fd4f6110017f14787d39fc648c694f67
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-06 14:27:59 +0700

    routing

--- create-nodejs/index.js
+++ create-nodejs/index.js
@@ -1,11 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    const myhtml = `
-    <h1>Hello Node.js</h1>
-    <p style="color:red">Brown studio  24</p>`
-    res.write(myhtml)
-    res.end()
+    const pathName = req.url
+    console.log("url = ", pathName);
+    if(pathName ==="/" || pathName === "/home"){
+        res.end("Hello Brown")
+    }else if(pathName==="/product"){
+        res.end("Hello Project")
+    }else{
+        res.writeHead(404)
+        res.end("Not Found")
+    }
 }).listen(3000, 'localhost', () => {
     console.log("start server in port 3000");
 })
