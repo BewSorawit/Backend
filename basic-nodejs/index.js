49040459e71513ad5575ee53af55bab84d872797
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-06 13:43:38 +0700

    asyschronous file

--- basic-nodejs/index.js
+++ basic-nodejs/index.js
@@ -1,12 +1,16 @@
-// Blocking
+// Non-Blocking
 const fs = require('fs')
 
 // read input.txt
-const data = fs.readFileSync('myFile/input.txt','utf-8')
-console.log(data);
+fs.readFile('myFile/input.txt', 'utf-8', (err, data) => {
+    if (err) {
+        return console.log("เกิดข้อผิดพลาด ", err);
+    }
+    const outputText = `Hello Node.js\n${data}\nไฟล์นี้ถูกเขียนเมื่${new Date}`
+    fs.writeFile('myFile/output.txt', outputText, err => {
+        if (err) {
+            return console.log("เกิดข้อผิดพลาด ", err);
+        }
+    })
+})
 console.log("จบการทำงาน");
-
-// เขียนไฟล์
-const outputText = `Hello Node.js\n${data}\nไฟล์ถูกเขียนเมื่อ ${new Date()}`
-fs.writeFileSync("myFile/output.txt",outputText)
-console.log("เขียนไฟล์เรียบร้อย");
