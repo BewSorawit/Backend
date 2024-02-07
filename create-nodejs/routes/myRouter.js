934a1f295eca3bb471a25247ed8fac6d6a3f3923
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-07 17:22:31 +0700

    router parameter

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -7,8 +7,17 @@ router.get('/',(req,res)=>{
     res.type('text/html')
     res.sendFile(path.join(__dirname,'../templates/index.html'))
 })
-router.get("/product",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../templates/product1.html"))
+router.get("/product/:id",(req,res)=>{
+    const productId = req.params.id
+    if(productId === "1"){
+        res.sendFile(path.join(__dirname,"../templates/product1.html"))
+    }else if(productId === "2"){
+        res.sendFile(path.join(__dirname,"../templates/product2.html"))
+    }else if(productId === "3"){
+        res.sendFile(path.join(__dirname,"../templates/product3.html"))
+    }else{
+        res.redirect('/')
+    }
 })
 
 module.exports = router
