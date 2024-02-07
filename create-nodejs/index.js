28938e86b5039153883a29de9cef04ef8f9cdef9
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-07 16:59:44 +0700

    express & routing

--- create-nodejs/index.js
+++ create-nodejs/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use((req,res)=>{
+app.get('/',(req,res)=>{
     res.send("Hello Express.js")
 })
-
+app.get("/product",(req,res)=>{
+    res.send("Hello Product")
+})
 app.listen(8080,()=>{
     console.log("run server at port 8080");
 })
