6660562625f4da61f1e0278354fc421347578b87
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 00:03:43 +0700

    Views & Template
    ejs

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
 
-
+router.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
 
 module.exports = router
