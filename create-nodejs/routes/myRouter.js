ea65dd80885a9acf04e4fd1225bfdcc332a340b8
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 00:10:47 +0700

    send data to templates

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.render('index.ejs')
+    const name = "Brown"
+    const age = 35
+    const address = "<h1>BANK</h1>"
+    res.render('index',{name:name,age:age, address:address})
+    
 })
 
 module.exports = router
