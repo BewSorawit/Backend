71d8c89493b5c71449de6b22cfc729308d976801
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 00:31:31 +0700

    array ejs

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -2,11 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    const name = "Brown"
-    const age = 15
-    const address = "<h1>BANK</h1>"
-    const order = 10
-    res.render('index',{name:name,age:age, address:address,order:order})
+    const product = ["เสื้อ","พัดลม","หูฟัง","คีย์บอร์ด"]
+    res.render('index',{product:product})
     
 })
 
