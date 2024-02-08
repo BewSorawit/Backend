2a5bc9c78b905ab4edf010fa4a5e5279ab4915c5
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-08 23:47:41 +0700

    static file

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -2,22 +2,6 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-router.get('/',(req,res)=>{
-    res.status(200)
-    res.type('text/html')
-    res.sendFile(path.join(__dirname,'../templates/index.html'))
-})
-router.get("/product/:id",(req,res)=>{
-    const productId = req.params.id
-    if(productId === "1"){
-        res.sendFile(path.join(__dirname,"../templates/product1.html"))
-    }else if(productId === "2"){
-        res.sendFile(path.join(__dirname,"../templates/product2.html"))
-    }else if(productId === "3"){
-        res.sendFile(path.join(__dirname,"../templates/product3.html"))
-    }else{
-        res.redirect('/')
-    }
-})
+
 
 module.exports = router
