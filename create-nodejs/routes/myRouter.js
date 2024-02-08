4cd0b7bfacc194f3e2c1bb5934d5a47d3090c738
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 00:15:33 +0700

    ejs if-else

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/',(req,res)=>{
     const name = "Brown"
-    const age = 35
+    const age = 15
     const address = "<h1>BANK</h1>"
     res.render('index',{name:name,age:age, address:address})
     
