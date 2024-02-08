3c987129243ec02b5504378690c91390b3fcca1f
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 00:21:23 +0700

    ejs loop

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -5,7 +5,8 @@ router.get('/',(req,res)=>{
     const name = "Brown"
     const age = 15
     const address = "<h1>BANK</h1>"
-    res.render('index',{name:name,age:age, address:address})
+    const order = 10
+    res.render('index',{name:name,age:age, address:address,order:order})
     
 })
 
