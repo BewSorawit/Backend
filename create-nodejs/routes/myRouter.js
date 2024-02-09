0805a5f9c7c3249d8b597572898718444d6f2ae0
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 18:53:27 +0700

    อัพโหลดไฟล์ด้วย multer

--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Product = require('../models/product')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images/products')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '.jpg')
+    }
+})
+
+const upload = multer({ storage: storage })
 
 router.get('/', async (req, res) => {
     try {
@@ -20,12 +32,12 @@ router.get('/manage', (req, res) => {
     res.render('manage')
 })
 
-router.post('/insert', async (req, res) => {
+router.post('/insert',upload.single('image'), async (req, res) => {
     try {
         let data = new Product({
             name: req.body.name,
             price: req.body.price,
-            image: req.body.image,
+            image: req.file.filename,
             description: req.body.description
         });
         await data.save();
