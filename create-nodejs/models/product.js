3e1e31623976f1e052e052f1c13e7bfda6ae9527
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-02-09 12:16:39 +0700

    บันทึกข้อมูล

--- create-nodejs/models/product.js
+++ create-nodejs/models/product.js
@@ -1,19 +1,18 @@
-const { mongoose } = require('mongoose')
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb://localhost:27017/productDB';
 
-const dbUrl = 'mongodb://localhost:27017/productDB'
+mongoose.connect(dbUrl).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => console.log(err));
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(err => console.log(err))
 
 let productSchema = mongoose.Schema({
     name: String,
     price: Number,
     image: String,
     description: String
-})
+});
 
-let Product = mongoose.model("product", productSchema)
+let Product = mongoose.model("Product", productSchema);
 
-module.exports = Product
+module.exports = Product;
--- create-nodejs/routes/myRouter.js
+++ create-nodejs/routes/myRouter.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const product = require('../models/product')
+const Product = require('../models/product')
 
-router.get('/', (req, res) => {
-    const products = [
-        { name: "โน๊ตบุ๊ค", price: 25500, image: "images/products/product1.png" },
-        { name: "เสื้อผ้า", price: 500, image: "images/products/product2.png" },
-        { name: "พัดลม", price: 200, image: "images/products/product3.png" },
-    ]
-    res.render('index', { products: products })
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.render('index', { products: products })
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+
 })
 
 router.get('/addForm', (req, res) => {
@@ -18,8 +20,20 @@ router.get('/manage', (req, res) => {
     res.render('manage')
 })
 
-router.post('/insert', (req, res) => {
-    console.log(req.body.name);
-    res.render('form')
-})
+router.post('/insert', async (req, res) => {
+    try {
+        let data = new Product({
+            name: req.body.name,
+            price: req.body.price,
+            image: req.body.image,
+            description: req.body.description
+        });
+        await data.save();
+        res.redirect('/');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 module.exports = router
